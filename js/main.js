0dbd6c5ba9d73c37b3d2902c04cb2c7b88e1aa6a
tinoferrucci <constantinoferrucci@gmail.com>
2023-05-14 22:58:41 -0300

    mostrar productos

--- js/main.js
+++ js/main.js
@@ -4,10 +4,13 @@ var muebles = [];
 // Llamada a una API donde se ubican todos los muebles y sus atributos
 fetch("https://sheetdb.io/api/v1/42qk3gwtfyppb")
   .then((res) => res.json())
-  .then((responses) =>
-    responses.forEach((mueble) => {
-      muebles.unshift(mueble);
-    })
-  );
+  .then((muebles) => mostrarMuebles(muebles));
 
-// Inicializamos un objeto Vue
+function mostrarMuebles(muebles) {
+  var body = "";
+  for (mueble of muebles) {
+    body += `<tr><td>${mueble.id}</td><td>${mueble.nombre}</td><td>${mueble.stock}</td><td>${mueble.precio}</td></tr>`;
+  }
+
+  document.getElementById("muebles").innerHTML = body;
+}
